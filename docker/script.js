f4e87c7118afc1b14fa915682930766d4fe341fb
Ayush20076564 <20076564@mydbs.ie>
2025-11-07 18:39:57 +0000

    Basic HTML and CSS page for testing

---
+++ docker/script.js
@@ -0,0 +1,4 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    const message = document.getElementById("demo");
+    message.textContent = " Hello to DevOps Project ";
+});
